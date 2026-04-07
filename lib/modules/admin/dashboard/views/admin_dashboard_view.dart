9f17b12f414b6805398a73be1a3c1bf051833551
IvanoWoii <rezaivano123@gmail.com>
2026-04-07 18:22:52 +0700

    chore: update dependencies in pubspec.lock

--- lib/modules/admin/dashboard/views/admin_dashboard_view.dart
+++ lib/modules/admin/dashboard/views/admin_dashboard_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 import 'package:get/get.dart';
 import '../../../../core/constants/app_colors.dart';
 import '../controllers/admin_dashboard_controller.dart';
